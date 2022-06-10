1446f1e26faf2bed9999e8b27bf1299a908a63c2
fazt <fazt@faztweb.com>
2022-06-09 23:27:04 -0500

    updated packages

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
-const express = require("express");
-const morgan = require("morgan");
-const path = require("path");
+import express from "express";
+import morgan from "morgan";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+// Routes
+import indexRoutes from "./routes/index.js";
+
+// Initialize express
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // settings
 app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "views"));
+app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // middlewares
 app.use(morgan("dev"));
 
 // routes
-app.use(require("./routes"));
+app.use(indexRoutes);
 
 // static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(join(__dirname, "public")));
 
 // listening the Server
-app.listen(app.get("port"), () => {
-  console.log("Server on port", app.get("port"));
-});
+app.listen(app.get("port"));
+console.log("Server on port", app.get("port"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const router = express.Router();
+import { Router } from "express";
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'First Web Node' });
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "First Web Node" });
 });
 
-router.get('/contact', (req, res) => {
-  res.render('contact', { title: 'Contact Page' });
+router.get("/about", (req, res) => {
+  res.render("about", { title: "About First Node Website" });
 });
 
-module.exports = router;
+router.get("/contact", (req, res) => {
+  res.render("contact", { title: "Contact Page" });
+});
+
+export default router;
